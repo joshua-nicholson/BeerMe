908262afcf4c0f00ff27ceff97636565d264b5b9
Matthew Compton <matthewc174@gmail.com>
2017-02-16 16:04:56 -0500

    setting up some of the buttons on main activity for login

--- app/src/main/java/xyz/beerme/beerme/MainActivity.java
+++ app/src/main/java/xyz/beerme/beerme/MainActivity.java
@@ -2,12 +2,68 @@ package xyz.beerme.beerme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonLogin;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView signupLink;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void onClick(View v) {
+        if (v.equals("@+id/google_button")) {
+            AuthUI.IdpConfig googleIdp = new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER)
+                    .setPermissions(Arrays.asList(Scopes.GAMES))
+                    .build();
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setProviders(Arrays.asList(new IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                    googleIdp,
+                                    new IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+                            .build(),
+                    RC_SIGN_IN);
+
+        }
+
+        if (v.equals("@+id/facebook_button")) {
+
+            AuthUI.IdpConfig facebookIdp = new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER)
+                    .setPermissions(Arrays.asList("user_friends"))
+                    .build();
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                    facebookIdp))
+                            .build(),
+                    RC_SIGN_IN);
+
+        }
+
+        if (v.equals("@+id/email_button")) {
+
+        }
+
+    }
+
 }
